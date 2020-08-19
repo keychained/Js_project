3e725d174fb8e49bf0a7fe84c87bdae6ac467665
keychained <kihoonchoi91@gmail.com>
2020-08-18 20:45:02 -0400

    minor changes

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -2,16 +2,13 @@ function setup() {
     var canvas = createCanvas(800, 600);
     canvas.parent("canvas-container");
     background(200);
-    // fill(200);
-    // rect(0, 0, width, 50);
-    // fill(0);
-    // text("Play Again", 350, 20, width / 2); 
 }
 
 function draw() {
     fill(200);
     rect(0, 0, width, 50);
     fill(0);
+    stroke(1);
     text("Play Again", 350, 20, width / 2); 
 }
 
@@ -23,6 +20,8 @@ function mousePressed() {
 
 function mouseDragged() {
     if (mouseY > 55) {
+    fill(255);
+    noStroke();
     ellipse(mouseX, mouseY, 20, 20);
     }
 }
