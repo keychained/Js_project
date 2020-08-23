43c7017daa9d59900e946e5f956d4c001aac3fb3
keychained <kihoonchoi91@gmail.com>
2020-08-23 19:00:45 -0400

    flames added, still work in progress

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -1,22 +1,56 @@
-var x = 50
+let spritesheet;
+let spritedata;
+let animation = [];
 
 function preload() {
     bg = loadImage('images/windows-background.jpeg');
-    flame = createImg('images/fire.gif')
+    flames = createImg('images/fire_circles.gif');
+    spritesheet = loadImage('images/fire_circles_sprite.png');
+    spritedata = loadJSON('images/fire_circles.json');
 }
 
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("canvas-container");
     image(bg, 0, 0);
-}
-
-function draw() {
     fill(180);
     rect(0, 0, width, 50);
     fill(0);
     text("Play Again", 350, 20, width / 2);
     cursor('images/flamethrower.cur');
+    let frames = spritedata.frames;
+    for (let i = 0; i < frames.length; i ++) {
+        let pos = frames[i].position;
+        let img = spritesheet.get(pos.x, pos.y, pos.w, pos.h);
+        animation.push(img);
+    }
+    console.log(animation);
+}
+
+function draw() {
+    if (mouseIsPressed && mouseY > 30 && mouseY < 610) {
+    image(animation[frameCount % animation.length], mouseX - 30, mouseY - 10);
+    }
+    // fill(220, 12);
+    // image(bg, 0, 0);
+    // bg(0, 0, 800, 800);
+    // fill(0);
+    // noStroke();
+    // ellipse(mouseX, mouseY, 60, 60);
+    // image(bg, 0, 0);
+    // fill(180);
+    // rect(0, 0, width, 50);
+    // fill(0);
+    // text("Play Again", 350, 20, width / 2);
+    // cursor('images/flamethrower.cur');
+    // image(bg, 0, 0);
+    // if (mouseY > 40 && mouseY < 640 && mouseX > 30 && mouseX < 850 && mouseIsPressed) {
+    //     fill(0, 10);
+    //     noStroke();
+    //     fill(255);
+    //     ellipse(mouseX - 60, mouseY, 30, 30);
+    //     flames.position(mouseX + 480, mouseY + 50);
+    // }
 }
 
 function mousePressed() {
@@ -25,12 +59,19 @@ function mousePressed() {
     }
 }
 
-function mouseDragged() {
-    if (mouseY > 70 && mouseY < 630) {
-    fill(255);
-    noStroke();
-    image(flame, mouseX - 60, mouseY - 30, x, x);
-    // flame.position(mouseX + 500, mouseY + 80)
-    }
-    
-}
+// function mouseDragged() {
+//     if (mouseY > 40 && mouseY < 640 && mouseX > 30 && mouseX < 850) {
+//         fill(0, 20);
+//         noStroke();
+//         // ellipse(mouseX - 60, mouseY, 30, 30);
+//         // image(animation[frameCount % animation.length], mouseX, mouseY);
+//         // flames.position(mouseX + 520, mouseY);
+//     }
+// }
+
+// function mouseReleased() {
+//     if (mouseY > 40 && mouseY < 640 && mouseX > 30 && mouseX < 850) {
+//         // image(animation[frameCount % animation.length], mouseX, mouseY);
+//        flames.position(mouseX + 520, mouseY, 1, 1);
+//     }
+// }
