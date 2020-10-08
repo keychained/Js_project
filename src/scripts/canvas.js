2133ecfd80175c3c913deae62eff0c92cb2afecf
keychained <kihoonchoi91@gmail.com>
2020-10-07 21:34:07 -0400

    added effects

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -3,8 +3,8 @@ let spritedata;
 let fire_animation;
 let animations = [];
 let backgrounds = [];
-let bButton;
-let gButton;
+let fires;
+let firesss;
 
 function preload() {
   bg1 = loadImage("images/windows-background.jpeg");
@@ -15,42 +15,21 @@ function preload() {
   fire_animation = loadAnimation(spritesheet);
   fire_animation.frameDelay = 0.5;
   backgrounds = [bg1, bg2, bg3, bg4];
-    randomButton = createButton("Change Background!");
-    // b2Button = createButton("Background 2");
-    // b3Button = createButton("Background 3");
-    // b4Button = createButton("Background 4");
-    randomButton.parent("buttons");
-    // b2Button.parent("buttons");
-    // b3Button.parent("buttons");
-    // b4Button.parent("buttons");
-    randomButton.mousePressed(randomBackground);
-    // b2Button.mousePressed(background2);
-    // b3Button.mousePressed(background3);
-    // b4Button.mousePressed(background4);
+  randomButton = createButton("Change Background!");
+  randomButton.parent("buttons");
+  randomButton.mousePressed(randomBackground);
 }
 
 function setup() {
   var canvas = createCanvas(800, 550);
   canvas.parent("canvas-container");
   background = random(backgrounds);
-  // b1Button = createButton("Background 1");
-  // b2Button = createButton("Background 2")
-  // b3Button = createButton("Background 3");
-  // b4Button = createButton("Background 4");
-  // b1Button.parent("buttons");
-  // b2Button.parent("buttons");
-  // b3Button.parent("buttons");
-  // b4Button.parent("buttons");
-  // b1Button.mousePressed(background1);
-  // b2Button.mousePressed(background2);
-  // b3Button.mousePressed(background3);
-  // b4Button.mousePressed(background4);
   cursor("images/flamethrower.cur");
-  // let frames = spritedata.frames;
-  // for (let i = 0; i < frames.length; i++) {
-  //   let pos = frames[i].position;
-  //   let img = spritesheet1.get(pos.x, pos.y, pos.w, pos.h);
-  //   animations.push(img);
+  fires = new Group();
+  // for (let i = 0; i < 20; i++) {
+  //   let fire = createSprite(random(0, width), random(0, height));
+  //   fire.addAnimation("floating", fire_animation);
+  //   fires.add(fire);
   // }
 }
 
@@ -58,21 +37,25 @@ function draw() {
   image(background, 0, 0);
   if (mouseIsPressed) {
     animation(fire_animation, mouseX, mouseY);
+    let fire = createSprite(mouseX, mouseY);
+    fire.addAnimation("floating", fire_animation);
+    fires.add(fire);
   }
+  drawSprites(fires);
 }
 
 function randomBackground() {
   setup();
 }
 
-// function background2() {
-//   image(bg2, 0, 0);
-// }
-
-// function background3() {
-//   image(bg3, 0, 0);
-// }
-
-// function background4() {
-//   image(bg4, 0, 0);
+// function drawFire() {
+//   let firesss = getSprites(fires);
+//   for (let i = 0; i < firesss.length; i++) {
+//     let firess = firesss[i];
+//     if (firess.position.x == mouseX && firess.position.y == mouseY) {
+//       firess.visible = true;
+//     } else {
+//       firess.visible = false;
+//     }
+//   }
 // }
